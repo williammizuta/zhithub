61365c8943b1f51d178c12f261a7eba509f306a4
Francisco Sokol & William Seiti Mizuta <william.mizuta@gmail.com>
2013-04-30 13:15:05 -0300

    Putting zhit as dependency

--- project/Build.scala
+++ project/Build.scala
@@ -7,15 +7,18 @@ object ApplicationBuild extends Build {
   val appName         = "zhithub"
   val appVersion      = "1.0-SNAPSHOT"
 
+
   val appDependencies = Seq(
     // Add your project dependencies here,
     jdbc,
-    anorm
+    anorm,
+    "br.com.caelum" % "zhit" % "1.0-SNAPSHOT"
   )
 
 
-  val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
-  )
+   val main = play.Project(appName, appVersion, appDependencies).settings(
+    // Add your own project settings here
+     resolvers += "Local Maven Repository" at "file://"+Path.userHome.absolutePath+"/.m2/repository"
+   )
 
 }
